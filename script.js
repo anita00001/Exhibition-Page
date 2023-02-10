9e5bd2a6fb7f4af373b5db223c28b10b7dfac489
Anita <sharma.anita00001@gmail.com>
2023-02-10 18:13:54 +0545

    Update Committee Member Array

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const targetListItem4 = document.querySelector('#list-item4');
 const targetListItem5 = document.querySelector('#list-item5');
 const targetListItem6 = document.querySelector('#list-item6');
 
-
 targetMenuIcon.addEventListener('click', () => {
   targetMenuIcon.classList.toggle('fa-times');
   targetNavBar.classList.toggle('mobile-menu-navbar')
@@ -19,20 +18,14 @@ const targetListItem = [targetListItem1, targetListItem2, targetListItem3, targe
 
 for (let i = 0; i < targetListItem.length; i += 1) {
   targetListItem.forEach((items) => {
-    
-  items.addEventListener('click', () => {
-  targetMenuIcon.classList.toggle('fa-times');
-  targetMenuIcon.classList.remove('fa-times');
-  targetNavBar.classList.remove('mobile-menu-navbar')
-  targetMenuList.classList.remove('mobile-menu-list')
-
-});
-  })
+    items.addEventListener('click', () => {
+      targetMenuIcon.classList.toggle('fa-times');
+      targetMenuIcon.classList.remove('fa-times');
+      targetNavBar.classList.remove('mobile-menu-navbar')
+      targetMenuList.classList.remove('mobile-menu-list')
+    });
+  });
 }
-
-
-
-/// /// /// Committeee // //
 
 const targetMemberWrap = document.getElementById('member-wrap');
 const targetCommittee = document.getElementById('committee');
@@ -43,42 +36,42 @@ const memberArray = [
     photo: 'assets/m1.jpg',
     name: 'Kanchan Tamrakar',
     position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    detail: 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
   },
 
   {
     photo: 'assets/m2.jpg',
     name: 'Kiran Pokharel',
-    position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    position: 'Chairman, Computer Engineering House, Pulchowk Campus',
+    detail: 'Pokharel is the chairman of Computer Engineering House since 2019, and the organizer of "Coding and Decoding Challenge-2020"'
   },
 
   {
     photo: 'assets/m3.jpg',
     name: 'Nancy Bhattarai',
-    position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    position: 'Secretary, Robotics Club, Pulchowk Campus',
+    detail: 'Bhattarai is the ecretary of Robotics Club since 2021, and the organizer of "Footstep of Buddha-2022"'
   },
 
   {
     photo: 'assets/m4.jpg',
     name: 'Raj Upadhyay',
-    position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    position: 'Member, Computer Engineering House, Pulchowk Campus',
+    detail: 'Upadhyay is the member of Computer Engineering House since 2019, and the winner of "Coding and Decoding Challenge-2020"'
   },
 
   {
     photo: 'assets/m5.jpg',
     name: 'Nisha Paudel',
-    position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    position: 'Chairman, Civil Engineering Society, Pulchowk Campus',
+    detail: 'Paudel is the chairman of Civil Engineering Society since 2020, and the organizer of "Build Your Country-2021"'
   },
 
   {
     photo: 'assets/m6.jpg',
     name: 'Yahudi Tamang',
-    position: 'Chairman, Robotics Club, Pulchowk Campus',
-    detail : 'Tamrakar is the chairman of Robotics Club since 2020, and the organizer of "Footstep of Buddha-2022"'
+    position: 'Member, Civil Engineering Society, Pulchowk Campus',
+    detail: 'Tamrakar is the member of  Civil Engineering Society since 2020, and the organizer of "Build Your Country-2021"'
   },
 ]
 
@@ -98,8 +91,7 @@ for (let i = 0; i <= memberArray.length; i++) {
   </div>`
   targetMemberWrap.appendChild(divMember);
 
-  if(i>=2) {
+  if (i >= 2) {
     divMember.className = "member hide";
   }
-  
 }
